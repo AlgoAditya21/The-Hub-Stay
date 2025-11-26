2b6a1fa010a0492153ebf1a649f9c28fd729b9b2
Adityaraj Pal <paladityaraj353@gmail.com>
2025-11-27 04:33:52 +0530

    My 4th commit

--- routes/listing.js
+++ routes/listing.js
@@ -34,6 +34,10 @@ router.get("/new",(req,res)=>{
 router.get("/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id).populate("reviews");
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist!")
+        res.redirect("/listings");
+    }
     res.render("listings/show",{listing});
 })
 );
@@ -44,6 +48,7 @@ router.post("/",validateListing,wrapAsync(async(req,res,next)=>{
         const newListing=new Listing(req.body.listing);
         
         await newListing.save();
+        req.flash("success","New Listing Created!");
         res.redirect("/listings")
 })
 )
@@ -52,6 +57,10 @@ router.post("/",validateListing,wrapAsync(async(req,res,next)=>{
 router.get("/:id/edit",wrapAsync(async (req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist!")
+        res.redirect("/listings");
+    }
     res.render("listings/edit",{listing});
 })
 );
@@ -61,6 +70,7 @@ router.put("/:id",validateListing,wrapAsync(async(req,res)=>{
     
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    req.flash("success","Listing Updated!");
     res.redirect(`/listings/${id}`)
 })
 )
@@ -70,6 +80,7 @@ router.delete("/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let deletedListing=await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success","Listing deleted!");
     res.redirect("/listings");
 })
 );
